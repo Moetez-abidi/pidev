1837613c103c0096ef9f8bd145b0066b50bf99d4
Moetez-abidi <44264362+Moetez-abidi@users.noreply.github.com>
2020-06-10 09:36:56 +0100

    Add files via upload

---
+++ Solution.Data/MyContext.cs
@@ -0,0 +1,29 @@
+﻿using Solution.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Data
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("name=machaine")
+        {
+
+        }
+        //les dbsets
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //config + conventions
+            //modelBuilder.Configurations.Add(...);
+            //modelBuilder.Conventions.Add(...);
+        }
+    }
+}
